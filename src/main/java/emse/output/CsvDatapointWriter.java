6ee04f166627434ca1047fa7e842f30eb6d9ff33
Leon Zimmermann <leon.zimmermann@adesso.de>
2022-06-20 14:21:57 +0200

    DatapointWriter angepasst

--- src/main/java/emse/output/CsvDatapointWriter.java
+++ src/main/java/emse/output/CsvDatapointWriter.java
@@ -1,9 +1,14 @@
 package emse.output;
 
+import emse.models.Method;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import emse.models.Datapoint;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class CsvDatapointWriter implements DatapointWriter {
 
@@ -11,8 +16,21 @@ public class CsvDatapointWriter implements DatapointWriter {
 
     @Override
     public void writeDatapoint(final Datapoint datapoint) {
+        final boolean fileAlreadyExists = new File(OUTPUT_FILE).exists();
         try (FileWriter writer = new FileWriter(OUTPUT_FILE, true)) {
-            writer.append(String.valueOf(datapoint.method.ordinal())).append(";")
+            // write header
+            if (!fileAlreadyExists) {
+                writer.append(
+                    Arrays.stream(datapoint.getClass().getFields()).map(Field::getName).collect(
+                        Collectors.joining(";"))).append("\n");
+            }
+            // write data
+            writer.append(String.valueOf(datapoint.id)).append(";")
+                    .append(String.valueOf(datapoint.method.ordinal())).append(";")
+                    .append(String.valueOf(datapoint.complexity)).append(";")
+                    .append(String.valueOf(datapoint.numberOfConjunctions)).append(";")
+                    .append(String.valueOf(datapoint.numberOfParameters)).append(";")
+                    .append(datapoint.result ? "1" : "0").append(";")
                     .append(String.valueOf(datapoint.timeInMillis)).append(";")
                     .append(datapoint.correct ? "1" : "0")
                     .append("\n")
